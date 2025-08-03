308d02a517ab3b4780d6efc0134bf69c1e425cc8
Anonymous User <golobal@gmail.com>
2025-08-03 12:25:27 +0100

    fix: Remove SSL configuration settings from Nginx setup

--- setup.sh
+++ setup.sh
@@ -405,16 +405,6 @@ server {
     ssl_certificate ${CERT_PATH};
     ssl_certificate_key ${KEY_PATH};
 
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_prefer_server_ciphers on;
-    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
-    ssl_session_timeout 1d;
-    ssl_session_cache shared:SSL:10m;
-    ssl_session_tickets off;
-
-    resolver 8.8.8.8 8.8.4.4 valid=300s;
-    resolver_timeout 5s;
-
     location / {
         proxy_pass http://127.0.0.1:8030;
         proxy_http_version 1.1;
